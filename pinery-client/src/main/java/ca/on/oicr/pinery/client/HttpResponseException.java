b7ef8388e9db387b0d5765d930fc54b3f68ef56b
Dillan Cooke <dillan.cooke@oicr.on.ca>
2015-07-20 13:51:07 -0400

    added HttpResponseException javadocs

--- pinery-client/src/main/java/ca/on/oicr/pinery/client/HttpResponseException.java
+++ pinery-client/src/main/java/ca/on/oicr/pinery/client/HttpResponseException.java
@@ -2,6 +2,12 @@ package ca.on.oicr.pinery.client;
 
 import javax.ws.rs.core.Response.Status;
 
+/**
+ * Exception class to encapsulate any HTTP response status other than 200 (OK)
+ * 
+ * @author dcooke
+ *
+ */
 public class HttpResponseException extends Exception {
 	
 	private static final long serialVersionUID = -7430326891153732863L;
@@ -13,17 +19,39 @@ public class HttpResponseException extends Exception {
 		this(null, -1);
 	}
 	
+	/**
+	 * Constructs an HttpResponseException representing the specified status code
+	 * 
+	 * @param status the HTTP status code
+	 */
 	public HttpResponseException(int status) {
 		this(null, status);
 	}
 	
+	/**
+	 * Constructs an HttpResponseException representing an unknown status code returned from the 
+	 * specified URL
+	 * 
+	 * @param requestUrl
+	 */
+	public HttpResponseException(String requestUrl) {
+		this(requestUrl, -1);
+	}
+	
+	/**
+	 * Constructs an HttpResponseException representing the specified status code returned from 
+	 * the specified URL
+	 * 
+	 * @param requestUrl the URL requested
+	 * @param status the HTTP status code
+	 */
 	public HttpResponseException(String requestUrl, int status) {
 		this.status = Status.fromStatusCode(status);
 		this.requestUrl = requestUrl;
 	}
 	
 	@Override
-	public String toString() {
+	public String getMessage() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("HTTP Request");
 		if (requestUrl != null) {
@@ -33,12 +61,16 @@ public class HttpResponseException extends Exception {
 		if (status != null) {
 			sb.append(" with status ")
 				.append(status.getStatusCode())
-				.append(": ")
+				.append(" -- ")
 				.append(status.getReasonPhrase());
 		}
 		return sb.toString();
 	}
-	
+
+	/**
+	 * @return the status that caused this exception. Contains the status code, family, and 
+	 * reason phrase 
+	 */
 	public Status getStatus() {
 		return status;
 	}
@@ -47,6 +79,9 @@ public class HttpResponseException extends Exception {
 		this.status = status;
 	}
 	
+	/**
+	 * @return the url requested, which returned the status code that caused this exception
+	 */
 	public String getRequestUrl() {
 		return requestUrl;
 	}
