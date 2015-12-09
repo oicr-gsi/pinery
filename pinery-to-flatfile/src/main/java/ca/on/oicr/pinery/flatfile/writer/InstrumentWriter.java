c491f7ac51f849a1f5f203a5e1e8796f65c32815
Dillan Cooke <dillan.cooke@oicr.on.ca>
2015-12-09 16:05:34 -0500

    removed TODO comment for fixed bug

--- pinery-to-flatfile/src/main/java/ca/on/oicr/pinery/flatfile/writer/InstrumentWriter.java
+++ pinery-to-flatfile/src/main/java/ca/on/oicr/pinery/flatfile/writer/InstrumentWriter.java
@@ -44,7 +44,7 @@ public class InstrumentWriter extends Writer {
   @Override
   protected String[] getRecord(int row) {
     InstrumentDto instrument = instruments.get(row);
-    InstrumentModelDto model = models.get(instrument.getModelId()); // TODO: modelId null
+    InstrumentModelDto model = models.get(instrument.getModelId());
     
     String[] data = {
         instrument.getId().toString(),
