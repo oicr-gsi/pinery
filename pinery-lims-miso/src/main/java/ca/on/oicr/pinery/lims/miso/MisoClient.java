7bad8f7709e0d464ce1dd7632c81f641fbaf6e87
Dillan Cooke <dillan.cooke@oicr.on.ca>
2017-02-17 13:11:05 -0500

    get Library created time from changelog

--- pinery-lims-miso/src/main/java/ca/on/oicr/pinery/lims/miso/MisoClient.java
+++ pinery-lims-miso/src/main/java/ca/on/oicr/pinery/lims/miso/MisoClient.java
@@ -253,7 +253,7 @@ public class MisoClient implements Lims {
       "        ,NULL tissueType\n" + 
       "        ,p.shortName project\n" + 
       "        ,lai.archived archived\n" + 
-      "        ,l.creationDate created\n" + 
+      "        ,lcl.creationDate created\n" + 
       "        ,lclcu.userId createdById\n" + 
       "        ,lcl.lastUpdated modified\n" + 
       "        ,lcluu.userId modifiedById\n" + 
@@ -323,7 +323,7 @@ public class MisoClient implements Lims {
       "LEFT JOIN BoxPosition pos ON pos.targetId = l.libraryId\n" + 
       "        AND pos.targetType LIKE 'Library%'\n" +
       "LEFT JOIN Box box ON box.boxId = pos.boxId\n" + 
-      "LEFT JOIN (SELECT libraryId, MAX(changeTime) as lastUpdated from LibraryChangeLog GROUP BY libraryId) lcl\n" +
+      "LEFT JOIN (SELECT libraryId, MAX(changeTime) as lastUpdated, MIN(changeTime) as creationDate from LibraryChangeLog GROUP BY libraryId) lcl\n" +
       "        ON lai.libraryId = lcl.libraryId\n" +
       "LEFT JOIN (SELECT userId, libraryId FROM LibraryChangeLog lcl1 WHERE changeTime = (\n" +
       "        SELECT MIN(lcl2.changeTime) FROM LibraryChangeLog lcl2 where lcl1.libraryId = lcl2.libraryId)\n" +
