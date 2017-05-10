07935d4bf046d692a46330a47118c63d4e8f910f
Andre Masella <andre.masella@oicr.on.ca>
2017-05-10 13:17:21 -0400

    Add Prometheus monitoring to Pinery

---
+++ pinery-ws/src/main/java/ca/on/oicr/pinery/ws/component/PineryContextListener.java
@@ -0,0 +1,33 @@
+package ca.on.oicr.pinery.ws.component;
+
+import java.io.IOException;
+
+import javax.management.MalformedObjectNameException;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.prometheus.client.hotspot.DefaultExports;
+import io.prometheus.jmx.JmxCollector;
+
+public class PineryContextListener implements ServletContextListener {
+  private static final Logger log = LoggerFactory.getLogger(PineryContextListener.class);
+
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {
+  }
+
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+    // Export all JVM HotSpot stats to Prometheus
+    DefaultExports.initialize();
+    try {
+      new JmxCollector(event.getServletContext().getResource("classpath:tomcat-prometheus.yml").getFile()).register();
+    } catch (MalformedObjectNameException | IOException e) {
+      log.error("Failed to load Prometheus configuration.", e);
+    }
+  }
+
+}
