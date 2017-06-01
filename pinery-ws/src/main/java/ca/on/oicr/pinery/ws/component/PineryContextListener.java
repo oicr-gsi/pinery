fcfeeffd6f1a7e9421c85dc0a7e9d5737f5dfa7f
Dillan Cooke <dillan.cooke@oicr.on.ca>
2017-06-01 15:10:22 -0400

    fixed Prometheus setup

--- pinery-ws/src/main/java/ca/on/oicr/pinery/ws/component/PineryContextListener.java
+++ pinery-ws/src/main/java/ca/on/oicr/pinery/ws/component/PineryContextListener.java
@@ -1,5 +1,6 @@
 package ca.on.oicr.pinery.ws.component;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
@@ -7,15 +8,11 @@ import javax.management.MalformedObjectNameException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.prometheus.client.hotspot.DefaultExports;
 import io.prometheus.jmx.JmxCollector;
 
 public class PineryContextListener implements ServletContextListener {
-  private static final Logger log = LoggerFactory.getLogger(PineryContextListener.class);
-
+  
   @Override
   public void contextDestroyed(ServletContextEvent event) {
   }
@@ -25,12 +22,14 @@ public class PineryContextListener implements ServletContextListener {
     // Export all JVM HotSpot stats to Prometheus
     DefaultExports.initialize();
     try {
-      URL yamlConfig = event.getServletContext().getResource("classpath:tomcat-prometheus.yml");
-      if (yamlConfig != null) {
-        new JmxCollector(yamlConfig.getFile()).register();
+      URL yamlConfig = Thread.currentThread().getContextClassLoader().getResource("tomcat-prometheus.yml");
+      if (yamlConfig == null) {
+        throw new IllegalStateException("Prometheus configuration not found");
       }
+      
+      new JmxCollector(new File(yamlConfig.getFile())).register();
     } catch (MalformedObjectNameException | IOException e) {
-      log.error("Failed to load Prometheus configuration.", e);
+      throw new IllegalStateException("Failed to load Prometheus configuration.", e);
     }
   }
 
