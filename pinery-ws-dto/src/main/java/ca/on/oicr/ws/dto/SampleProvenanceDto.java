3205f7fbf54979e6cad84cd359559d427de3b9ec
Dillan Cooke <dillan.cooke@oicr.on.ca>
2018-11-21 11:51:21 -0500

    fixup: ignore unknown json properties to improve backwards compatibility

--- pinery-ws-dto/src/main/java/ca/on/oicr/ws/dto/SampleProvenanceDto.java
+++ pinery-ws-dto/src/main/java/ca/on/oicr/ws/dto/SampleProvenanceDto.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import ca.on.oicr.gsi.provenance.model.SampleProvenance;
 
@@ -15,6 +16,7 @@ import ca.on.oicr.gsi.provenance.model.SampleProvenance;
  *
  * @author mlaszloffy
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SampleProvenanceDto implements SampleProvenance {
 
     private String studyTitle;
