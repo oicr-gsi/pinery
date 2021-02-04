04da636f8b1c0d0d6ecd04536b8ce4b722613f9d
Dillan Cooke <dillan.cooke@oicr.on.ca>
2021-02-04 17:22:43 -0500

    removed PowerMock

--- pinery-client/src/test/java/ca/on/oicr/pinery/client/PineryClientTest.java
+++ pinery-client/src/test/java/ca/on/oicr/pinery/client/PineryClientTest.java
@@ -1,12 +1,7 @@
 package ca.on.oicr.pinery.client;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Invocation;
@@ -14,34 +9,41 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import org.jboss.resteasy.plugins.providers.jackson.ResteasyJackson2Provider;
-import org.junit.Rule;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(PineryClient.class)
 public class PineryClientTest {
 
-  @Rule public final ExpectedException exception = ExpectedException.none();
+  @Mock private Client clientMock;
+  @Mock private WebTarget targetMock;
+
+  private PineryClient sut;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    when(clientMock.register(ResteasyJackson2Provider.class)).thenReturn(clientMock);
+    sut = new PineryClient("http://pretend.pinery.server/", clientMock);
+  }
+
+  @After
+  public void tearDown() {
+    sut.close();
+  }
 
   @Test
   public void testCallPinery() throws HttpResponseException {
-    Client clientMock = makeMockClient();
-    WebTarget targetMock = mock(WebTarget.class);
     Invocation.Builder builderMock = mock(Invocation.Builder.class);
     Response responseMock = Response.ok("response").build();
     when(clientMock.target("http://pretend.pinery.server/test")).thenReturn(targetMock);
     when(targetMock.request()).thenReturn(builderMock);
     when(builderMock.get()).thenReturn(responseMock);
 
-    Response response = null;
-    try (PineryClient client = new PineryClient("http://pretend.pinery.server/")) {
-      response = client.callPinery("test");
-    }
+    Response response = sut.callPinery("test");
+
     assertNotNull(response);
     assertEquals(200, response.getStatus());
     assertEquals("response", response.getEntity());
@@ -49,18 +51,14 @@ public class PineryClientTest {
 
   @Test
   public void testCallPineryNull() throws HttpResponseException {
-    Client clientMock = makeMockClient();
-    WebTarget targetMock = mock(WebTarget.class);
     Invocation.Builder builderMock = mock(Invocation.Builder.class);
     Response responseMock = Response.ok(null).build();
     when(clientMock.target("http://pretend.pinery.server/test")).thenReturn(targetMock);
     when(targetMock.request()).thenReturn(builderMock);
     when(builderMock.get()).thenReturn(responseMock);
 
-    Response response = null;
-    try (PineryClient client = new PineryClient("http://pretend.pinery.server/")) {
-      response = client.callPinery("test");
-    }
+    Response response = sut.callPinery("test");
+
     assertNotNull(response);
     assertEquals(200, response.getStatus());
     assertNull(response.getEntity());
@@ -68,160 +66,112 @@ public class PineryClientTest {
 
   @Test
   public void testCallPineryStatusNotOk() throws HttpResponseException {
-    Client clientMock = makeMockClient();
-    WebTarget targetMock = mock(WebTarget.class);
     Invocation.Builder builderMock = mock(Invocation.Builder.class);
     Response responseMock = Response.status(Status.INTERNAL_SERVER_ERROR).entity(null).build();
     when(clientMock.target("http://pretend.pinery.server/test")).thenReturn(targetMock);
     when(targetMock.request()).thenReturn(builderMock);
     when(builderMock.get()).thenReturn(responseMock);
 
-    try (PineryClient client = new PineryClient("http://pretend.pinery.server/")) {
-      exception.expect(HttpResponseException.class);
-      client.callPinery("test");
-    }
-  }
-
-  private Client makeMockClient() {
-    Client clientMock = mock(Client.class);
-    when(clientMock.register(ResteasyJackson2Provider.class)).thenReturn(clientMock);
-
-    PowerMockito.mockStatic(PineryClient.class);
-    when(PineryClient.getInsecureClient()).thenReturn(clientMock);
-    when(PineryClient.getSecureClient()).thenReturn(clientMock);
-
-    return clientMock;
+    assertThrows(HttpResponseException.class, () -> sut.callPinery("test"));
   }
 
   @Test
   public void testOpenClose() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertTrue(client.isOpen());
-    assertFalse(client.isClosed());
+    assertTrue(sut.isOpen());
+    assertFalse(sut.isClosed());
 
-    client.close();
-    assertFalse(client.isOpen());
-    assertTrue(client.isClosed());
+    sut.close();
+    assertFalse(sut.isOpen());
+    assertTrue(sut.isClosed());
   }
 
   @Test
   public void testGetSample() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertNotNull(client.getSample());
-    assertNotNull(client.getSample());
+    assertNotNull(sut.getSample());
+    assertNotNull(sut.getSample());
 
-    client.close();
-    exception.expect(IllegalStateException.class);
-    client.getSample();
+    sut.close();
+    assertThrows(IllegalStateException.class, () -> sut.getSample());
   }
 
   @Test
   public void testGetSampleProject() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertNotNull(client.getSampleProject());
-    assertNotNull(client.getSampleProject());
+    assertNotNull(sut.getSampleProject());
+    assertNotNull(sut.getSampleProject());
 
-    client.close();
-    exception.expect(IllegalStateException.class);
-    client.getSampleProject();
+    sut.close();
+    assertThrows(IllegalStateException.class, () -> sut.getSampleProject());
   }
 
   @Test
   public void testGetSampleType() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertNotNull(client.getSampleType());
-    assertNotNull(client.getSampleType());
+    assertNotNull(sut.getSampleType());
+    assertNotNull(sut.getSampleType());
 
-    client.close();
-    exception.expect(IllegalStateException.class);
-    client.getSampleType();
+    sut.close();
+    assertThrows(IllegalStateException.class, () -> sut.getSampleType());
   }
 
   @Test
   public void testGetSequencerRun() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertNotNull(client.getSequencerRun());
-    assertNotNull(client.getSequencerRun());
+    assertNotNull(sut.getSequencerRun());
+    assertNotNull(sut.getSequencerRun());
 
-    client.close();
-    exception.expect(IllegalStateException.class);
-    client.getSequencerRun();
+    sut.close();
+    assertThrows(IllegalStateException.class, () -> sut.getSequencerRun());
   }
 
   @Test
   public void testGetUser() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertNotNull(client.getUser());
-    assertNotNull(client.getUser());
+    assertNotNull(sut.getUser());
+    assertNotNull(sut.getUser());
 
-    client.close();
-    exception.expect(IllegalStateException.class);
-    client.getUser();
+    sut.close();
+    assertThrows(IllegalStateException.class, () -> sut.getUser());
   }
 
   @Test
   public void testGetAttributeName() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertNotNull(client.getAttributeName());
-    assertNotNull(client.getAttributeName());
+    assertNotNull(sut.getAttributeName());
+    assertNotNull(sut.getAttributeName());
 
-    client.close();
-    exception.expect(IllegalStateException.class);
-    client.getAttributeName();
+    sut.close();
+    assertThrows(IllegalStateException.class, () -> sut.getAttributeName());
   }
 
   @Test
   public void testGetChangeLog() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertNotNull(client.getChangeLog());
-    assertNotNull(client.getChangeLog());
+    assertNotNull(sut.getChangeLog());
+    assertNotNull(sut.getChangeLog());
 
-    client.close();
-    exception.expect(IllegalStateException.class);
-    client.getChangeLog();
+    sut.close();
+    assertThrows(IllegalStateException.class, () -> sut.getChangeLog());
   }
 
   @Test
   public void testGetInstrument() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertNotNull(client.getInstrument());
-    assertNotNull(client.getInstrument());
+    assertNotNull(sut.getInstrument());
+    assertNotNull(sut.getInstrument());
 
-    client.close();
-    exception.expect(IllegalStateException.class);
-    client.getInstrument();
+    sut.close();
+    assertThrows(IllegalStateException.class, () -> sut.getInstrument());
   }
 
   @Test
   public void testGetInstrumentModel() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertNotNull(client.getInstrumentModel());
-    assertNotNull(client.getInstrumentModel());
+    assertNotNull(sut.getInstrumentModel());
+    assertNotNull(sut.getInstrumentModel());
 
-    client.close();
-    exception.expect(IllegalStateException.class);
-    client.getInstrumentModel();
+    sut.close();
+    assertThrows(IllegalStateException.class, () -> sut.getInstrumentModel());
   }
 
   @Test
   public void testGetOrder() {
-    makeMockClient();
-    PineryClient client = new PineryClient("http://pretend.pinery.server/", true);
-    assertNotNull(client.getOrder());
-    assertNotNull(client.getOrder());
+    assertNotNull(sut.getOrder());
+    assertNotNull(sut.getOrder());
 
-    client.close();
-    exception.expect(IllegalStateException.class);
-    client.getOrder();
+    sut.close();
+    assertThrows(IllegalStateException.class, () -> sut.getOrder());
   }
 }
